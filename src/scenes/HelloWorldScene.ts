8e856e809077c336263c987ec94b1b66678a6ff5
Grzegorz Ptak <ptaq321@gmail.com>
2022-03-02 14:53:54 +0100

    initial release

---
+++ src/scenes/HelloWorldScene.ts
@@ -0,0 +1,13 @@
+import Phaser from "phaser";
+
+export default class HelloWorldScene extends Phaser.Scene {
+  constructor() {
+    super("hello-world");
+  }
+
+  preload() {}
+
+  create() {}
+
+  update() {}
+}
